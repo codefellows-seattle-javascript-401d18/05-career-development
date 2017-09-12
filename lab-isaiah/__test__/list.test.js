7a0b7b036c3fd0bce635c48f642570eac262c5b6
lonewalker72 <walkisai.72@gmail.com>
2017-09-12 15:09:12 -0700

    redo because I lost everything of lab completion

--- lab-isaiah/__test__/list.test.js
+++ lab-isaiah/__test__/list.test.js
@@ -12,5 +12,41 @@ describe('Testing List Module:', function() {
       expect(this.list.length).toBe(0);
       done();
     });
+    test('Should create new list:', () => {
+
+    });
+    test('Should create new list:', () => {
+
+    });
+  });
+
+  describe('#mypush', () => {
+    test('Should return (a, b, c, d) given (a, b, c) and d', () => {
+
+    });
+  });
+
+  describe('#mypop', () => {
+    test('Should return (a, b, c) given (a, b, c, d):', () => {
+
+    });
+  });
+
+  describe('#myfilter', () => {
+    test('Should return 10, 20, 30, 40, 50 given (2, 4, 6, 7, 8, 9, 10, 20, 30, 40, 50):', () => {
+
+    });
+  });
+
+  describe('#myreduce', () => {
+    test('Should return 10 given (1, 2, 3, 4):', () => {
+
+    });
+  });
+
+  describe('#myslice', () => {
+    test('Should return [3, 4] given (1, 2, 3, 4, 5, 6):', () => {
+
+    });
   });
 });
